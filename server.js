6008a5f580d6be6cd58b0a57766a6fefc1d8f9bd
MariaSchinoni <maria.schinoni10@gmail.com>
2022-11-11 10:55:59 -0300

    user creation

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const ShortUrl = require("./models/shortUrl");
+const User = require("./models/User");
+const bcrypt = require("bcryptjs");
+
+const cors = require("cors");
+
 const app = express();
+
+app.use(cors());
+
+app.use(express.json());
 
 mongoose.connect(
   "mongodb+srv://mariaemila:tub4r40@cluster0.e1cn3fh.mongodb.net/?retryWrites=true&w=majority",
@@ -33,6 +42,41 @@ app.get("/:shortUrl", async (req, res) => {
   shortUrl.save();
 
   res.redirect(shortUrl.full);
+});
+
+app.post("/registerUser", async (req, res) => {
+  const { name, email, password } = req.body;
+  console.log(req.body);
+
+  const foundEmail = await User.findOne({ email: email });
+  console.log("foundemail:", foundEmail);
+  if (foundEmail?.email) {
+    return res.sendStatus(400);
+  }
+
+  const passwordHash = await bcrypt.hash(password, 8);
+
+  await User.create({ name, email, password: passwordHash });
+  return res.sendStatus(201);
+});
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    console.log("entrei no if do user");
+    return res.sendStatus(404);
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password);
+
+  if (!passwordMatch) {
+    console.log("entrei no if do password");
+    return res.sendStatus(404);
+  }
+
+  return res.sendStatus(200);
 });
 
 app.listen(process.env.PORT || 5000);
